40b4e0c0b63b1d964f1599190d0887470f57a3b6
Sandeep Paritala <sandeepkumarparitala@gmail.com>
2019-06-30 15:44:37 +0530

    added form submit

--- src/Components/Register/index.js
+++ src/Components/Register/index.js
@@ -52,105 +52,111 @@ class Registerform extends Component {
     return (
       <LoginWrapper>
         <div className="wrapper animate">
-          <div className="container">
-            <p>
-              <b>Register</b>
-            </p>
-            <div className="name-section">
-              <div className="firstname">
-                <label className="label">First name</label>
-                <input
-                  name="firstName"
-                  placeholder="Enter your first name"
-                  required
-                  value={firstName}
-                  onChange={this.handleInputChanges}
-                />
-              </div>
-              <div className="last-name">
-                <label>Last name</label>
-                <input
-                  name="lastName"
-                  placeholder="Enter last name"
-                  required
-                  value={lastName}
-                  onChange={this.handleInputChanges}
-                />
-              </div>
-            </div>
-            <div className="phn-section">
-              <div className="number">
-                <label>Number</label>
-                <div className="number-inputs">
+          <form onSubmit={this.handleSubmit}>
+            <div className="container">
+              <p>
+                <b>Register</b>
+              </p>
+              <div className="name-section">
+                <div className="firstname">
+                  <label className="label">First name</label>
                   <input
-                    style={{ width: "15%", marginRight: "10px" }}
-                    name="countryCode"
-                    placeholder="+91"
+                    name="firstName"
+                    placeholder="Enter your first name"
                     required
-                    value={countryCode}
+                    value={firstName}
                     onChange={this.handleInputChanges}
+                    required
                   />
+                </div>
+                <div className="last-name">
+                  <label>Last name</label>
                   <input
-                    name="phoneNumber"
-                    placeholder="Enter phone number"
+                    name="lastName"
+                    placeholder="Enter last name"
                     required
-                    value={phoneNumber}
+                    value={lastName}
                     onChange={this.handleInputChanges}
+                    required
                   />
                 </div>
               </div>
-            </div>
-            <label>collage</label>
-            <input
-              placeholder="Enter collage name"
-              name="collage"
-              value={collage}
-              onChange={this.handleInputChanges}
-              required
-            />
-            <label>Email</label>
-            <input
-              placeholder="Enter Email"
-              name="email"
-              value={email}
-              onChange={this.handleInputChanges}
-              required
-            />
-            <label>Password</label>
-            <input
-              type="password"
-              name="password"
-              placeholder="Enter Password"
-              value={password}
-              onChange={this.handleInputChanges}
-              required
-            />
+              <div className="phn-section">
+                <div className="number">
+                  <label>Number</label>
+                  <div className="number-inputs">
+                    <input
+                      style={{ width: "15%", marginRight: "10px" }}
+                      name="countryCode"
+                      placeholder="+91"
+                      required
+                      value={countryCode}
+                      onChange={this.handleInputChanges}
+                      required
+                    />
+                    <input
+                      name="phoneNumber"
+                      placeholder="Enter phone number"
+                      required
+                      value={phoneNumber}
+                      onChange={this.handleInputChanges}
+                      required
+                    />
+                  </div>
+                </div>
+              </div>
+              <label>collage</label>
+              <input
+                placeholder="Enter collage name"
+                name="collage"
+                value={collage}
+                onChange={this.handleInputChanges}
+                required
+              />
+              <label>Email</label>
+              <input
+                placeholder="Enter Email"
+                name="email"
+                value={email}
+                onChange={this.handleInputChanges}
+                required
+              />
+              <label>Password</label>
+              <input
+                type="password"
+                name="password"
+                placeholder="Enter Password"
+                value={password}
+                onChange={this.handleInputChanges}
+                required
+              />
 
-            <label>Repeat Password</label>
-            <input
-              type="password"
-              name="confirmPassword"
-              placeholder="Repeat Password"
-              value={confirmPassword}
-              onChange={this.handleInputChanges}
-              required
-            />
-            <hr />
-            <p>
-              By creating an account you agree to our{" "}
-              <a href="#">Terms & Privacy</a>.
-            </p>
-
-            <button name="submit" className="registerbtn">
-              Register
-            </button>
-
-            <div className="signin">
+              <label>Repeat Password</label>
+              <input
+                type="password"
+                name="confirmPassword"
+                placeholder="Repeat Password"
+                value={confirmPassword}
+                onChange={this.handleInputChanges}
+                required
+              />
+              <hr />
               <p>
-                Already have an account? <Link to="/login">Sign in</Link>.
+                By creating an account you agree to our{" "}
+                <a href="#">Terms & Privacy</a>.
               </p>
+
+              <button name="submit" className="registerbtn">
+                Register
+              </button>
+
+              <div className="signin">
+                <p>
+                  Already have an account? <Link to="/login">Sign in</Link>.
+                </p>
+              </div>
             </div>
-          </div>
+          </form>
         </div>
       </LoginWrapper>
     );
